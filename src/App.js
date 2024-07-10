d2be5a8f7756ba5cb638de01779d69f12e3ab355
BadriVishalPadhy <128573881+BadriVishalPadhy@users.noreply.github.com>
2024-07-10 22:11:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { LoginSocialFacebook } from "reactjs-social-login";
+import { FacebookLoginButton } from "react-social-login-buttons";
+
+function App() {
+  const [profile, setProfile] = useState(null);
+
+  return (
+    <div>
+      {!profile ? (
+        <LoginSocialFacebook
+          appId="1151187802854140"
+          onResolve={(response) => {
+            console.log(response);
+            setProfile(response.data);
+          }}
+          onReject={(error) => {
+            console.log(error);
+          }}
+        >
+          <FacebookLoginButton />
+        </LoginSocialFacebook>
+      ) : (
+        ""
+      )}
+
+      {profile ? (
+        <div>
+          <h1>{profile.name}</h1>
+          <img src={profile.picture.data.url} alt="Profile Picture"/>
+          <div>{profile.page}</div>
+        </div>
+      ) : (
+        " "
+      )}
+    </div>
+  );
+}
+
+export default App;
